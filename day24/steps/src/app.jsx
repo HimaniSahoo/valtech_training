303cb169c10bee8b50bb16276769b59d059709a0
HimaniSahoo <91072757+HimaniSahoo@users.noreply.github.com>
2022-10-13 18:14:03 +0530

    Add files via upload

---
+++ day24/steps/src/app.jsx
@@ -0,0 +1,9 @@
+import Users from "./user.component"
+
+let App = ()=>{
+    return <div className="container">
+        <h1>Ajax with react hook</h1>
+        <Users/>
+    </div>
+}
+export default App
---
+++ day24/steps/src/user.component.jsx
@@ -0,0 +1,127 @@
+import { useEffect } from "react"
+import axios from "axios";
+import { useState } from "react";
+ 
+let Users = ()=>{
+    let [hero, setHeroes] = useState([]);
+    let [nhero, createHero] = useState({ title : '', firstname : '', lastname : ''});
+    let [ehero, modifyHero] = useState({ _id:'', title : '', firstname : '', lastname : ''});
+    let [toggle, toggleHandler] = useState(false);
+ 
+    let refresh = ()=>{
+        axios.get("http://localhost:5050/data").then(res => {
+            setHeroes(res.data);
+        })
+    }
+ 
+    useEffect(function(){
+       refresh();
+    },[]);
+ 
+    let addHero = ()=>{
+        axios.post("http://localhost:5050/data",nhero)
+        .then(res => {
+            createHero({ title : '', firstname : '', lastname : ''})
+            refresh();
+        })
+        .catch(err => console.log("Error ", err))
+    }
+    let clickHandler = (evt)=>{
+        createHero({...nhero, [evt.target.id] : evt.target.value})
+    }
+    let clickUpdateHandler = (evt)=>{
+        modifyHero({...ehero, [evt.target.name] : evt.target.value})
+    }
+    let editHero = (hid)=>{
+        axios.get("http://localhost:5050/edit/"+hid).then(res => {
+            modifyHero(res.data);
+            toggleHandler(true);
+        })
+    }
+ 
+    let updateHero = () =>{
+       axios.post("http://localhost:5050/update/"+ehero._id, ehero)
+       .then(res => {
+        refresh();
+        modifyHero({ _id:'', title : '', firstname : '', lastname : ''});
+        toggleHandler(false);
+       } )
+       .catch(err => console.log("Error ", err))
+    };
+ 
+    let deleteHero = (hid)=>{
+       //  console.log(hid);
+       axios.delete("http://localhost:5050/delete/"+hid)
+       .then( res => refresh())
+       .catch(err => console.log(err));
+    }
+    return <div>
+                <h2>Heroes List</h2>
+                { !toggle && <div>
+                    <h3>Create Hero</h3>
+                    <div className="mb-3">
+                        <label htmlFor="title" className="form-label">Title</label>
+                        <input value={nhero.title} onInput={(evt)=> clickHandler(evt) } className="form-control" id="title" />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="firstname" className="form-label">First Name</label>
+                        <input value={nhero.firstname} onInput={(evt)=> clickHandler(evt) } className="form-control" id="firstname" />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="lastname" className="form-label">Last Name</label>
+                        <input value={nhero.lastname} onInput={(evt)=> clickHandler(evt) } className="form-control" id="lastname" />
+                    </div>
+                    <button onClick={addHero} type="submit" className="btn btn-primary">Submit</button>
+ 
+                </div>}
+ 
+                {
+                    toggle && <div>
+                    <h3>Update Hero</h3>
+                    <div className="mb-3">
+                        <label htmlFor="edit_title" className="form-label">Title</label>
+                        <input name="title" value={ehero.title} onInput={(evt)=> clickUpdateHandler(evt) } className="form-control" id="edit_title" />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="edit_firstname" className="form-label">First Name</label>
+                        <input name="firstname" value={ehero.firstname} onInput={(evt)=> clickUpdateHandler(evt) } className="form-control" id="edit_firstname" />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="edit_lastname" className="form-label">Last Name</label>
+                        <input name="lastname" value={ehero.lastname} onInput={(evt)=> clickUpdateHandler(evt) } className="form-control" id="edit_lastname" />
+                    </div>
+                    <button onClick={updateHero} type="submit" className="btn btn-primary">Update Hero</button>
+ 
+                </div>
+                }
+                <table className="table">
+                <thead>
+                    <tr>
+                    <th scope="col">Sl #</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">First Name</th>
+                    <th scope="col">Last Name</th>
+                    <th scope="col">Edit</th>
+                    <th scope="col">Delete</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        hero.map((hero, idx) =>{
+                            return <tr key={hero._id}>
+                                        <th scope="row">{ idx + 1 }</th>
+                                        <td>{ hero.title }</td>
+                                        <td>{ hero.firstname }</td>
+                                        <td>{ hero.lastname }</td>
+                                        <td><button onClick={()=> editHero(hero._id)} className="btn btn-warning">Edit</button></td>
+                                        <td><button onClick={()=> deleteHero(hero._id)} className="btn btn-danger">Delete</button></td>
+                                    </tr>
+                        })
+                    }
+                </tbody>
+                </table>
+            </div>
+}
+ 
+export default Users 
+ 
